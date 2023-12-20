ee82a75852f15f8e6b8b23368fbc5301631dce1c
SoufianeAmghar <soufiane35.live@gmail.com>
2023-12-20 12:54:17 +0100

    Added local variables and logged them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-os-env';
+  public environment = environment.environment;
+  public SomeAPIKey = environment.APIKeys.SomeAPIKey;
+  public SomeOtherAPIKey = environment.APIKeys.SomeOtherAPIKey;
+
+
+  constructor() {
+    console.log('Environment:', this.environment);
+    console.log('SomeAPIKey:', this.SomeAPIKey);
+    console.log('SomeOtherAPIKey:', this.SomeOtherAPIKey);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  environment: 'development',
+  APIKeys: {
+    SomeAPIKey: 'DEV API Key',
+    SomeOtherAPIKey: 'DEV API Key 2'
+  }
+};
